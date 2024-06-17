8a09787985eda56029191e89325bcaff85679eb0
aki <lala.akira@gmail.com>
2024-06-17 17:52:43 +0900

    refactor: typo

--- src/buildCompletionItems.ts
+++ src/buildCompletionItems.ts
@@ -2,9 +2,9 @@ import { CompletionItem, CompletionItemKind, MarkdownString } from "vscode";
 import { Table } from "./Parser";
 
 export default function buildCompletionItems(table: Table): CompletionItem[] {
-  return Array.from(table.columns.values()).map(colmun => {
-    const markdown = ["```ruby", colmun.lineText, "```"].join("\n");
-    const item = new CompletionItem(colmun.name, CompletionItemKind.Method);
+  return Array.from(table.columns.values()).map(column => {
+    const markdown = ["```ruby", column.lineText, "```"].join("\n");
+    const item = new CompletionItem(column.name, CompletionItemKind.Method);
     item.detail = table.className;
     item.documentation = new MarkdownString(markdown);
     return item;
